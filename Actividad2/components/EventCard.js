0449f493d5aee46288d1a6368a96277b80157d35
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2023-11-12 18:42:21 +0100

    Fecha formateada

--- Actividad2/components/EventCard.js
+++ Actividad2/components/EventCard.js
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export default function EventCard({event, onPress}) {
+
+  const formatDate = (date) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString('es-ES', {
+      minute: 'numeric',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric'
+    });
+
+  };
+
   return (
   <TouchableOpacity onPress={onPress} style={[styles.container]}>
     <Text >Nombre: {event.nombre}</Text>
     <Text >Precio: {event.precio}</Text>
     <Text >Ubicación: {event.ubicacion}</Text>
     <Text >Descripción: {event.descripcion}</Text>
-    <Text >Fecha inicio: {event.fecha_inicio}</Text>
-    <Text >Fecha fin: {event.fecha_fin}</Text>
+    <Text >Fecha inicio: {formatDate(event.fecha_inicio)}</Text>
+    <Text >Fecha fin: {formatDate(event.fecha_fin)}</Text>
   </TouchableOpacity>
   );
 }
