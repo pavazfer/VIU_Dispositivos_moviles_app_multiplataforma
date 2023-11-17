e7be9cb1740aadb6e9f4db8e79083a1aea773c1f
joaquintejero92 <joaquintejeroca@gmail.com>
2023-11-17 15:28:52 +0100

    Update CrearEvento.js

--- Actividad2/screens/CrearEvento.js
+++ Actividad2/screens/CrearEvento.js
@@ -1,92 +1,112 @@
-import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Pressable } from 'react-native';
 import { Formik } from 'formik';
 import { createEvent } from '../service/eventService';
 
 export default function CrearEvento({ navigation }) {
-  let initialValues = {
+  const initialValues = {
     nombre: '',
     ubicacion: '',
     precio: '',
     fechaInicio: '',
     fechaFin: '',
-    descripcion: ''
-  }
+    descripcion: '',
+  };
+
+  const [eventAdded, setEventAdded] = useState(false);
 
   const createNewEvent = (newEvent) => {
-    createEvent(newEvent).then(() =>
-      navigation.navigate({ name: 'Eventos', params: { title: 'refresh' } })
-    )
-  }
+    createEvent(newEvent).then(() => {
+      setEventAdded(true);
+      navigation.navigate({ name: 'Eventos', params: { title: 'refresh' } });
+    });
+  };
+
+  const navigateToCreateEvent = () => {
+    setEventAdded(false);
+    navigation.navigate({ name: 'CrearEvento' });
+  };
 
   return (
     <View style={styles.container}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => createNewEvent(values)}
-      >
+      <Formik initialValues={initialValues} onSubmit={(values) => createNewEvent(values)}>
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <View style={styles.formContainer}>
             <View>
-              <Text>Nombre del evento: </Text>
+              <Text style={styles.label}>Nombre del evento:</Text>
               <TextInput
                 onChangeText={handleChange('nombre')}
                 onBlur={handleBlur('nombre')}
                 value={values.nombre}
-                style={styles.input}
-              />
-            </View>
-            <View>
-              <Text>Precio: </Text>
-              <TextInput
-                onChangeText={handleChange('precio')}
-                onBlur={handleBlur('precio')}
-                value={values.precio}
-                style={styles.input}
-              />
-            </View>
-            <View>
-              <Text>Fehca inicio: </Text>
-              <TextInput
-                onChangeText={handleChange('fechaInicio')}
-                onBlur={handleBlur('fechaInicio')}
-                value={values.fechaInicio}
-                style={styles.input}
-              />
-            </View>
-            <View>
-              <Text>Fecha fin: </Text>
-              <TextInput
-                onChangeText={handleChange('fechaFin')}
-                onBlur={handleBlur('fechaFin')}
-                value={values.fechaFin}
-                style={styles.input}
-              />
-            </View>
-            <View>
-              <Text>Ubicación: </Text>
-              <TextInput
-                onChangeText={handleChange('ubicacion')}
-                onBlur={handleBlur('ubicacion')}
-                value={values.ubicacion}
-                style={styles.input}
-              />
-            </View>
-            <View>
-              <Text>Descripción: </Text>
-              <TextInput
-                onChangeText={handleChange('descripcion')}
-                onBlur={handleBlur('descripcion')}
-                value={values.descripcion}
+                placeholder="Nombre del evento"
                 style={styles.input}
               />
             </View>
 
-            <Pressable onPress={handleSubmit} style={[styles.btn, styles.createBtn]}>
+            <View>
+              <Text style={styles.label}>Precio:</Text>
+              <TextInput
+                onChangeText={handleChange('precio')}
+                onBlur={handleBlur('precio')}
+                value={values.precio}
+                placeholder="Precio"
+                style={styles.input}
+                keyboardType="numeric"
+              />
+            </View>
+
+            <View>
+              <Text style={styles.label}>Fecha inicio:</Text>
+              <TextInput
+                onChangeText={handleChange('fechaInicio')}
+                onBlur={handleBlur('fechaInicio')}
+                value={values.fechaInicio}
+                placeholder="YYYY-MM-DD HH:mm"
+                style={styles.input}
+              />
+            </View>
+
+            <View>
+              <Text style={styles.label}>Fecha fin:</Text>
+              <TextInput
+                onChangeText={handleChange('fechaFin')}
+                onBlur={handleBlur('fechaFin')}
+                value={values.fechaFin}
+                placeholder="YYYY-MM-DD HH:mm"
+                style={styles.input}
+              />
+            </View>
+
+            <View>
+              <Text style={styles.label}>Ubicación:</Text>
+              <TextInput
+                onChangeText={handleChange('ubicacion')}
+                onBlur={handleBlur('ubicacion')}
+                value={values.ubicacion}
+                placeholder="Ubicación"
+                style={styles.input}
+              />
+            </View>
+
+            <View>
+              <Text style={styles.label}>Descripción:</Text>
+              <TextInput
+                onChangeText={handleChange('descripcion')}
+                onBlur={handleBlur('descripcion')}
+                value={values.descripcion}
+                placeholder="Descripción"
+                style={[styles.input, styles.multilineInput]}
+                multiline
+              />
+            </View>
+
+            <Pressable onPress={handleSubmit} style={styles.btn}>
               <Text style={styles.createBtnText}>Añadir evento</Text>
             </Pressable>
           </View>
         )}
       </Formik>
+
     </View>
   );
 }
@@ -95,36 +115,53 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch'
+    alignItems: 'stretch',
   },
   formContainer: {
     marginHorizontal: 20,
     marginVertical: 10,
     flex: 1,
   },
+  label: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+  },
   input: {
     borderWidth: 1,
     borderColor: 'grey',
-    borderRadius: 3,
+    borderRadius: 5,
     paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginBottom: 10,
-    marginTop: 5
+    paddingVertical: 8,
+    marginBottom: 15,
+  },
+  multilineInput: {
+    minHeight: 80,
   },
   btn: {
-    borderRadius: 3,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
     alignItems: 'center',
-    marginHorizontal: 5,
-  },
-  createBtn: {
     backgroundColor: '#068a0e',
     marginTop: 15,
-    marginBottom: 5
+    marginBottom: 5,
+  },
+  addEventBtn: {
+    backgroundColor: '#086cc4',
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    borderRadius: 50,
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   createBtnText: {
     color: 'white',
-    fontSize: 16
-  }
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
+
+
