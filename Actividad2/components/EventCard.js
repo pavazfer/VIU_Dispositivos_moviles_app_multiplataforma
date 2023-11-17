c53ce95ba4038902e6f918e9f1c47f4c74843fcf
joaquintejero92 <joaquintejeroca@gmail.com>
2023-11-17 15:28:12 +0100

    Update EventCard.js

--- Actividad2/components/EventCard.js
+++ Actividad2/components/EventCard.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export default function EventCard({event, onPress}) {
 
+  
   const formatDate = (date) => {
     const newDate = new Date(date);
     return newDate.toLocaleDateString('es-ES', {
@@ -15,12 +16,13 @@ export default function EventCard({event, onPress}) {
 
   return (
   <TouchableOpacity onPress={onPress} style={styles.container}>
-    <Text><Text style={styles.title}>Nombre: </Text>{event.nombre}</Text>
-    <Text ><Text style={styles.title}>Precio: </Text>{event.precio}</Text>
-    <Text ><Text style={styles.title}>Ubicación: </Text>{event.ubicacion}</Text>
-    <Text ><Text style={styles.title}>Descripción: </Text>{event.descripcion}</Text>
-    <Text ><Text style={styles.title}>Fecha inicio: </Text>{formatDate(event.fecha_inicio)}</Text>
-    <Text ><Text style={styles.title}>Fecha fin: </Text>{formatDate(event.fecha_fin)}</Text>
+    <Text style={{ color: 'white', fontWeight: 600 }}>Nombre: {event.nombre}</Text>
+    <Text style={{ color: 'white', fontWeight: 600 }}>Precio: {event.precio}</Text>
+    <Text style={{ color: 'white', fontWeight: 600 }}>Ubicación: {event.ubicacion}</Text>
+    <Text style={{ color: 'white', fontWeight: 600 }}>Descripción: {event.descripcion}</Text>
+    <Text style={{ color: 'white', fontWeight: 600 }}>Fecha inicio: {formatDate(event.fecha_inicio)}</Text>
+    <Text style={{ color: 'white', fontWeight: 600 }}>Fecha fin: {formatDate(event.fecha_fin)}</Text>
+
   </TouchableOpacity>
   );
 }
@@ -28,15 +30,19 @@ export default function EventCard({event, onPress}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00569d',
     borderWidth: 1,
-    borderRadius: 3,
+    borderRadius: 40,
     borderColor: '#49494a',
     paddingHorizontal: 15,
     paddingVertical: 10,
-    marginVertical: 7
+    marginVertical: 20
   },
   title: {
-    fontWeight: 600
+    fontWeight: 600,
+    color: 'white'
+  },
+  text: {
+    color: 'white', // Puedes cambiar el color aquí
   }
 });
